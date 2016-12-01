251b4e66336243b11236b45673a6fcdbe1f29842
Eric Cornely <ericcornely@gmail.com>
2016-12-01 11:20:23 +0100

    Patched like proposed by @aakhmerov

--- src/test/java/be/ecornely/StartProcess.java
+++ src/test/java/be/ecornely/StartProcess.java
@@ -1,6 +1,7 @@
 package be.ecornely;
 
 import javax.ws.rs.client.ClientBuilder;
+
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -8,6 +9,7 @@ import javax.ws.rs.core.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.camunda.bpm.engine.impl.util.json.JSONArray;
 public class StartProcess {
   
   private static final ObjectMapper om = new ObjectMapper();
@@ -15,17 +17,19 @@ public class StartProcess {
   public static void main(String[] args) throws Exception {
     ObjectNode entity = om.createObjectNode();
     ObjectNode variables = entity.putObject("variables");
-    String demo = "[{\"network\":\"192.168.0.0\",\"mask\":\"24\",\"longmask\":\"255.255.255.0\",\"gateway\":\"192.168.0.1\",\"priority\":100},{\"network\":\"192.168.2.0\",\"mask\":\"24\",\"longmask\":\"255.255.255.0\",\"gateway\":\"192.168.2.1\",\"priority\":100}]";
+    //String demo = "[{\"network\":\"192.168.0.0\",\"mask\":\"24\",\"longmask\":\"255.255.255.0\",\"gateway\":\"192.168.0.1\",\"priority\":100},{\"network\":\"192.168.2.0\",\"mask\":\"24\",\"longmask\":\"255.255.255.0\",\"gateway\":\"192.168.2.1\",\"priority\":100}]";
+    JSONArray demo = new JSONArray("[{\"network\":\"192.168.0.0\",\"mask\":\"24\",\"longmask\":\"255.255.255.0\",\"gateway\":\"192.168.0.1\",\"priority\":100},{\"network\":\"192.168.2.0\",\"mask\":\"24\",\"longmask\":\"255.255.255.0\",\"gateway\":\"192.168.2.1\",\"priority\":100}]");
+    
     ObjectNode demoNode = variables.putObject("demo");
     demoNode.put("type", "Object");
     
     //set the value as a String
-    demoNode.put("value", demo);
+    demoNode.putPOJO("value", demo);
     
     //set the value as a json array
     //demoNode.set("value", om.readTree(demo));
     
-    demoNode.putObject("valueInfo").put("objectTypeName", "com.fasterxml.jackson.databind.node.ArrayNode").put("serializationDataFormat", "application/json");
+//    demoNode.putObject("valueInfo").put("objectTypeName", "com.fasterxml.jackson.databind.node.ArrayNode").put("serializationDataFormat", "application/json");
     
     System.out.printf("The request payload will be:%n%s%n%n", om.writerWithDefaultPrettyPrinter().writeValueAsString(entity));
     
